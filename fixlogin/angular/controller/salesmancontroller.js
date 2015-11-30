278595ffec605de7271eb607dba2f89cab943b74
projectku <piter@lipat.co.id>
2015-11-30 11:13:22 +0700

    Fix Login First

---
+++ fixlogin/angular/controller/salesmancontroller.js
@@ -0,0 +1,50 @@
+'use strict';
+myAppModule.controller("SalesmanController", ["$scope", "$location","$http", "authService", "auth", function ($scope, $location, $http, authService, auth) 
+{
+    $scope.userInfo = auth;
+    $http.get('http://api.lukisongroup.com/master/barangumums?access-token=azLSTAYr7Y7TLsEAML-LsVq9cAXLyAWa')
+    .success(function(data,status, headers, config) 
+    {
+        $scope.barangumums = data.BarangUmum ;
+    })
+
+    .error(function (data, status, header, config) 
+    {
+            
+    });
+
+    $http.get('http://api.lukisongroup.com/master/kategoris?access-token=azLSTAYr7Y7TLsEAML-LsVq9cAXLyAWa')
+    .success(function(data,status, headers, config) 
+    {
+        $scope.categories = data.Kategori ;
+    })
+
+    .error(function (data, status, header, config) 
+    {
+            
+    });
+
+    $http.get('http://api.lukisongroup.com/master/tipebarangs?access-token=azLSTAYr7Y7TLsEAML-LsVq9cAXLyAWa')
+    .success(function(data,status, headers, config) 
+    {
+        $scope.typebarangs = data.Tipebarang ;
+    })
+
+    .error(function (data, status, header, config) 
+    {
+            
+    });
+
+    $http.get('http://api.lukisongroup.com/master/supliers?access-token=azLSTAYr7Y7TLsEAML-LsVq9cAXLyAWa')
+    .success(function(data,status, headers, config) 
+    {
+        $scope.supliers = data.Suplier ;
+    })
+
+    .error(function (data, status, header, config) 
+    {
+            
+    });
+    
+}]);
+
