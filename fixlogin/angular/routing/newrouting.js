8dbfe9e189f2db5df0abefef94791b41614ae89f
projectku <piter@lipat.co.id>
2015-12-05 17:45:18 +0700

    Add Sabtu

--- fixlogin/angular/routing/newrouting.js
+++ fixlogin/angular/routing/newrouting.js
@@ -1,10 +1,10 @@
 'use strict';
 myAppModule.config(['$routeProvider', function($routeProvider,$authProvider)
 {
-	$routeProvider.when('/salesman/edit/barangumum/:idbarangumum',
+	$routeProvider.when('/salesman/new/barangumum',
 	{
-		templateUrl	: 'angular/partial/salesman/editbarangumum.html',
-		controller 	: 'EditBarangUmumController',
+		templateUrl	: 'angular/partial/salesman/newbarangumum.html',
+		controller 	: 'NewBarangUmumController',
 		resolve: 
 		{
             auth: function ($q, authService,$location) 
@@ -21,10 +21,10 @@ myAppModule.config(['$routeProvider', function($routeProvider,$authProvider)
             }
         }
 	});
-    $routeProvider.when('/salesman/edit/kategori/:idkategori',
+    $routeProvider.when('/salesman/new/kategori',
     {
-        templateUrl : 'angular/partial/salesman/editkategori.html',
-        controller  : 'EditKategoriController',
+        templateUrl : 'angular/partial/salesman/newkategori.html',
+        controller  : 'NewKategoriController',
         resolve: 
         {
             auth: function ($q, authService,$location) 
@@ -41,31 +41,10 @@ myAppModule.config(['$routeProvider', function($routeProvider,$authProvider)
             }
         }
     });
-    $routeProvider.when('/salesman/edit/suplier/:idsuplier',
+    $routeProvider.when('/salesman/new/suplier',
     {
-        templateUrl : 'angular/partial/salesman/editsuplier.html',
-        controller  : 'EditSuplierController',
-        resolve: 
-        {
-            auth: function ($q, authService,$location) 
-            {
-                var userInfo = authService.getUserInfo();
-                if (userInfo) 
-                {
-                    return $q.when(userInfo);
-                } 
-                else 
-                {
-                    $location.path('/');
-                }
-            }
-        }
-    });
-
-    $routeProvider.when('/salesman/edit/tipebarang/:idtipebarang',
-    {
-        templateUrl : 'angular/partial/salesman/edittipebarang.html',
-        controller  : 'EditTipeBarangController',
+        templateUrl : 'angular/partial/salesman/newsuplier.html',
+        controller  : 'NewSuplierController',
         resolve: 
         {
             auth: function ($q, authService,$location) 
@@ -83,10 +62,31 @@ myAppModule.config(['$routeProvider', function($routeProvider,$authProvider)
         }
     });
 
-    $routeProvider.when('/salesman/edit/barangunit/:idbarangunit',
+    $routeProvider.when('/salesman/new/tipebarang',
     {
-        templateUrl : 'angular/partial/salesman/editbarangunit.html',
-        controller  : 'EditBarangUnitController',
+        templateUrl : 'angular/partial/salesman/newtipebarang.html',
+        controller  : 'NewTipeBarangController',
+        resolve: 
+        {
+            auth: function ($q, authService,$location) 
+            {
+                var userInfo = authService.getUserInfo();
+                if (userInfo) 
+                {
+                    return $q.when(userInfo);
+                } 
+                else 
+                {
+                    $location.path('/');
+                }
+            }
+        }
+    });
+
+    $routeProvider.when('/salesman/new/barangunit',
+    {
+        templateUrl : 'angular/partial/salesman/newbarangunit.html',
+        controller  : 'NewTipeBarangController',
         resolve: 
         {
             auth: function ($q, authService,$location) 
